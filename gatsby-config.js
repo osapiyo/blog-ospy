8e234a4afea4496852fd6dba7e31e514a901d6a6
osapiyo <osapiyo1@gmail.com>
2022-04-17 14:17:46 +0900

    3rd commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,13 @@
-require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`,
-});
+// require("dotenv").config({
+//   path: `.env.${process.env.NODE_ENV}`,
+// });
 
-const contentfulConfig = {
-  spaceId: process.env.CONTENTFUL_SPACE_ID,
-  accessToken:
-    process.env.CONTENTFUL_ACCESS_TOKEN ||
-    process.env.CONTENTFUL_DELIVERY_TOKEN,
-};
+// const contentfulConfig = {
+//   spaceId: process.env.CONTENTFUL_SPACE_ID,
+//   accessToken:
+//     process.env.CONTENTFUL_ACCESS_TOKEN ||
+//     process.env.CONTENTFUL_DELIVERY_TOKEN,
+// };
 
 // If you want to use the preview API please define
 // CONTENTFUL_HOST and CONTENTFUL_PREVIEW_ACCESS_TOKEN in your
@@ -21,34 +21,41 @@ const contentfulConfig = {
 // https://www.contentful.com/developers/docs/references/content-preview-api/#/reference/spaces/space/get-a-space/console/js
 //
 // To change back to the normal CDA, remove the CONTENTFUL_HOST variable from your environment.
-if (process.env.CONTENTFUL_HOST) {
-  contentfulConfig.host = process.env.CONTENTFUL_HOST;
-  contentfulConfig.accessToken = process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN;
-}
+// if (process.env.CONTENTFUL_HOST) {
+//   contentfulConfig.host = process.env.CONTENTFUL_HOST;
+//   contentfulConfig.accessToken = process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN;
+// }
 
-const { spaceId, accessToken } = contentfulConfig;
+// const { spaceId, accessToken } = contentfulConfig;
 
-if (!spaceId || !accessToken) {
-  throw new Error(
-    "Contentful spaceId and the access token need to be provided."
-  );
-}
+// if (!spaceId || !accessToken) {
+//   throw new Error(
+//     "Contentful spaceId and the access token need to be provided."
+//   );
+// }
 
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Contentful Starter",
-    description: "Official Contentful Gatsby Starter",
+    title: 'Gatsby Contentful Starter',
+    description: 'Official Contentful Gatsby Starter',
   },
-  pathPrefix: "/gatsby-contentful-starter",
+  pathPrefix: '/gatsby-contentful-starter',
   plugins: [
-    "gatsby-transformer-remark",
-    "gatsby-transformer-sharp",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sharp",
-    "gatsby-plugin-image",
+    'gatsby-transformer-remark',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sharp',
+    'gatsby-plugin-image',
+    // {
+    //   resolve: 'gatsby-source-contentful',
+    //   options: contentfulConfig,
+    // },
     {
-      resolve: "gatsby-source-contentful",
-      options: contentfulConfig,
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId,
+        accessToken,
+      },
     },
   ],
-};
+}
